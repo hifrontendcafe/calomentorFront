72b2dd69ca6349335a8fc522a1c6618757db4689
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-10-11 17:22:14 -0300

    Update image domains

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "tailwindui.com",
-      "images.unsplash.com",
-      "cdn.discordapp.com",
-      "res.cloudinary.com",
-    ],
+    domains: ["cdn.discordapp.com", "res.cloudinary.com"],
   },
 };
