b53d5fe58e9e8e68ec0d48a7f5a48b22c3b5535b
Sebastián Córdoba <sebascordobaa@hotmail.com>
2021-11-04 23:19:07 -0300

    Update mentorship interface

--- src/interfaces/mentorship.interface.ts
+++ src/interfaces/mentorship.interface.ts
@@ -2,26 +2,22 @@ export interface IMentorhip {
   id: string;
   mentee_email: string;
   mentee_username_discord: string;
-  time_slot_id: string;
+  mentor_email: string;
   mentorship_status: string;
   mentor_id: string;
   mentor_name: string;
   info: string;
-  mentor_mail: string;
   mentee_name: string;
   tokenForCancel: string;
   mentee_id: string;
   who_cancel: string;
   cancel_cause: string;
+  feedback_mentee: string;
+  feedback_stars: number;
   time_slot_info: {
-    is_occupied: boolean;
-    date: number;
-    user_id: string;
-    is_cancelled: false;
-    tokenForCancel: string;
-    mentee_id: string;
-    mentee_username: string;
     id: string;
+    date: number;
+    is_occupied: boolean;
   };
 }
 
