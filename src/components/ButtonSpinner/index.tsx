7c49a91d7ee7a3979aaaf80109a80c92f39f0a03
Sebastián Córdoba <sebascordobaa@hotmail.com>
2022-03-20 17:25:43 -0300

    Add btn spinner component

---
+++ src/components/ButtonSpinner/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonSpinner = () => {
+  return (
+    <div className="flex items-center justify-center p-5">
+      <div className="w-4 h-4 border-b-2 rounded-full border-fecGreen animate-spin"></div>
+    </div>
+  );
+};
+
+export default ButtonSpinner;
