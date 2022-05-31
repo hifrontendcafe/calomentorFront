0c98b893b5272cb810c4c57ebe3967db9958eb97
Federico Minaya <elfedeminaya@gmail.com>
2022-05-31 18:58:39 -0300

    feat: add filters and loading status

--- src/pages/admin-history/index.tsx
+++ src/pages/admin-history/index.tsx
@@ -3,23 +3,21 @@ import DashboardLayout from '@/components/DashboardLayout';
 import { useRouter } from 'next/dist/client/router';
 import CustomHead from '@/components/CustomHead';
 import { IMentorship } from '@/interfaces/mentorship.interface';
-import HistoryMentorshipCardFromBot from '@/components/HistoryMentorshipCardFromBot';
 import {
   getAdminMentorshipHistory,
   getAdminMentorshipHistoryByName,
 } from '@/services/index';
-import GenericCard from '@/components/GenericCard';
 import useToastContext from '@/hooks/useToastContext';
 import CustomButton from '@/components/CustomButton';
 import { orderMentorshipsByDate } from '@/helpers/getOrderByDate';
 import { useNextAuthSession } from '@/hooks/useNextAuthSession';
 import { formatDate } from '@/helpers/formatDate';
+import Spinner from '@/components/Spinner';
 
 const AdminHistory: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [mentorships, setMentorships] = useState<IMentorship[]>([]);
   const [session, loading] = useNextAuthSession();
-  const noMentorships = mentorships?.length === 0;
   const router = useRouter();
   const { addToast } = useToastContext();
   const [name, setName] = useState<string>('');
@@ -27,13 +25,13 @@ const AdminHistory: React.FC = () => {
     id: string;
   } | null>(null);
 
-  const onSearchByName = async () => {
+  const onSearchByName = async (baseName?: string) => {
     setIsLoading(true);
     const {
       data: { data },
-    } = await getAdminMentorshipHistoryByName(name);
+    } = await getAdminMentorshipHistoryByName(baseName || name);
     setLastKey(null);
-    setMentorships(orderMentorshipsByDate(data));
+    setMentorships(orderMentorshipsByDate(data || []));
     setIsLoading(false);
   };
 
@@ -52,7 +50,7 @@ const AdminHistory: React.FC = () => {
     getAdminMentorshipHistory()
       .then(({ data: { data, lastKey } }) => {
         setLastKey(lastKey || null);
-        setMentorships(orderMentorshipsByDate(data));
+        setMentorships(orderMentorshipsByDate(data || []));
         setIsLoading(false);
         console.log('mentor', data);
       })
@@ -79,77 +77,145 @@ const AdminHistory: React.FC = () => {
     <>
       <CustomHead title="Historial Mentorías" />
       <DashboardLayout title="Historial Mentorías">
-        <div className="flex flex-col mt-8">
+        (
+        <div className="flex flex-col h-16 gap-2 mt-4">
+          <div className="flex w-1/2 gap-4">
+            <input
+              type="text"
+              id="name"
+              autoComplete="off"
+              placeholder="Buscar por nombre de mentee o mentor"
+              className="custom_input"
+              value={name}
+              onChange={({ target: { value } }) => setName(value)}
+              onKeyDown={e => {
+                if (e.key === 'Enter') {
+                  onSearchByName();
+                }
+              }}
+            />
+            <CustomButton
+              className="mt-1"
+              bntLabel={'Buscar'}
+              primary
+              clickAction={onSearchByName}
+              isActive={true}
+            />
+            <CustomButton
+              className="mt-1"
+              bntLabel={'Todos'}
+              primary
+              clickAction={getAllMentorships}
+              isActive={true}
+            />
+          </div>
+        </div>
+        )
+        {!isLoading && mentorships.length === 0 && (
+          <div
+            className={
+              'border-green-500 mx-4 flex items-center justify-between my-5 border bg-cardContentLight rounded-md'
+            }
+          >
+            <div className="flex-1 px-4 py-2 text-sm truncate">
+              <p className="py-2 text-center text-mainTextColor">
+                No hay mentorias registradas
+              </p>
+            </div>
+          </div>
+        )}
+        {isLoading && <Spinner />}
+        <div className="flex flex-col">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-700">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-300 sm:pl-6 md:pl-0 "
-                    >
-                      Fecha de carga
-                    </th>
-                    <th
-                      scope="col"
-                      className="py-3.5 px-3 text-left text-sm font-semibold text-gray-300"
-                    >
-                      Mentor
-                    </th>
-                    <th
-                      scope="col"
-                      className="py-3.5 px-3 text-left text-sm font-semibold text-gray-300"
-                    >
-                      Mentee
-                    </th>
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6 md:pr-0"
-                    >
-                      <span className="sr-only">Mas info</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="border-b border-gray-700">
-                  {mentorships?.map(mentorship => (
-                    <tr
-                      className="border-b border-gray-700"
-                      key={mentorship.id}
-                    >
-                      <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-300 whitespace-nowrap sm:pl-6 md:pl-0">
-                        {mentorship.mentorship_create_date}
-                      </td>
-                      <td className="px-3 py-4 text-sm text-gray-300 whitespace-nowrap">
-                        <div className="font-bold hover:text-teal-500">
-                          {mentorship.mentor_name}
-                        </div>
-                        <div className="text-gray-400">
-                          ID {mentorship.mentor_id}
-                        </div>
-                      </td>
-                      <td className="px-3 py-4 text-sm text-gray-300 whitespace-nowrap">
-                        <div className="font-bold hover:text-teal-500">
-                          {mentorship.mentee_name}
-                        </div>
-                        <div className="text-gray-400">
-                          ID {mentorship.mentee_id}
-                        </div>
-                      </td>
-                      <td className="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6 md:pr-0">
-                        <a
-                          href="#"
-                          className="text-mainTextColor hover:text-teal-600"
-                        >
-                          Remover registro
-                          <span className="sr-only"></span>
-                        </a>
-                      </td>
+              {!isLoading && mentorships.length > 0 && (
+                <table className="min-w-full divide-y divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-300 sm:pl-6 md:pl-0 "
+                      >
+                        Fecha de carga
+                      </th>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-3 text-left text-sm font-semibold text-gray-300"
+                      >
+                        Mentor
+                      </th>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-3 text-left text-sm font-semibold text-gray-300"
+                      >
+                        Mentee
+                      </th>
+                      <th
+                        scope="col"
+                        className="relative py-3.5 pl-3 pr-4 sm:pr-6 md:pr-0"
+                      >
+                        <span className="sr-only">Mas info</span>
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="border-b border-gray-700">
+                    {mentorships?.map(mentorship => (
+                      <tr
+                        className="border-b border-gray-700"
+                        key={mentorship.id}
+                      >
+                        <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-300 whitespace-nowrap sm:pl-6 md:pl-0">
+                          {formatDate(+mentorship.mentorship_create_date!)}
+                        </td>
+                        <td className="px-3 py-4 text-sm text-gray-300 whitespace-nowrap">
+                          <div
+                            onClick={() => {
+                              setName(mentorship.mentor_name);
+                              onSearchByName(mentorship.mentor_name);
+                            }}
+                            className="font-bold cursor-pointer hover:text-teal-500"
+                          >
+                            {mentorship.mentor_name}
+                          </div>
+                          <div className="text-gray-400">
+                            ID {mentorship.mentor_id}
+                          </div>
+                        </td>
+                        <td className="px-3 py-4 text-sm text-gray-300 whitespace-nowrap">
+                          <div className="font-bold hover:text-teal-500">
+                            {mentorship.mentee_name}
+                          </div>
+                          <div className="text-gray-400">
+                            ID {mentorship.mentee_id}
+                          </div>
+                        </td>
+                        <td className="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6 md:pr-0">
+                          <a
+                            href="#"
+                            className="text-mainTextColor hover:text-teal-600"
+                          >
+                            Remover registro
+                            <span className="sr-only"></span>
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </div>
+
+            {lastKey && (
+              <div className="flex justify-center w-full">
+                <CustomButton
+                  className="flex justify-center w-48 mt-1"
+                  bntLabel={'Buscar más'}
+                  primary
+                  clickAction={onSearchMore}
+                  isActive={true}
+                />
+              </div>
+            )}
           </div>
         </div>
       </DashboardLayout>
