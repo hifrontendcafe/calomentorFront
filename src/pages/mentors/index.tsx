dfaff450ff9ea4c25fd86bb58f21c66c3b214958
Francisco Peralta <fran.mper@gmail.com>
2022-05-31 20:12:10 -0300

    add filters and some data to mentors list

--- src/pages/mentors/index.tsx
+++ src/pages/mentors/index.tsx
@@ -8,10 +8,13 @@ import { getAllMentors } from '@/services';
 import GenericCard from '@/components/GenericCard';
 import useToastContext from '@/hooks/useToastContext';
 import { useNextAuthSession } from '@/hooks/useNextAuthSession';
+import CustomButton from '@/components/CustomButton';
 
 const Mentors = () => {
   const [mentors, setMentors] = useState<Mentor[]>([]);
+  const [filteredMentors, setFilteredMentors] = useState<Mentor[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [activeFilter, setActiveFilter] = useState<UserStatus | 'all'>('all');
   const [session, loading] = useNextAuthSession();
   const router = useRouter();
   const emptyMentors = mentors.length === 0;
@@ -33,6 +36,7 @@ const Mentors = () => {
             return 1;
           });
           setMentors(mentorsSorted);
+          setFilteredMentors(mentorsSorted);
           setIsLoading(false);
         })
         .catch(() => {
@@ -47,6 +51,40 @@ const Mentors = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loading, router]);
 
+  const onFilterChange = (filter: UserStatus | 'all') => {
+    const filterMentors = (status: UserStatus) => {
+      setFilteredMentors(mentors.filter(mentor => mentor.status === status));
+    };
+    setActiveFilter(filter);
+
+    if (filter === 'all') {
+      setFilteredMentors(mentors);
+    } else if (filter === UserStatus.ACTIVE) {
+      filterMentors(UserStatus.ACTIVE);
+    } else if (filter === UserStatus.INACTIVE) {
+      filterMentors(UserStatus.INACTIVE);
+    } else if (filter === UserStatus.OUT) {
+      filterMentors(UserStatus.OUT);
+    } else {
+      filterMentors(UserStatus.NOT_AVAILABLE);
+    }
+  };
+
+  const getActiveButton = (status: UserStatus | 'all') => {
+    if (status === activeFilter) {
+      return true;
+    }
+    return false;
+  };
+
+  const filterButtons = [
+    { label: 'Todos', filterName: 'all' },
+    { label: 'Activos', filterName: UserStatus.ACTIVE },
+    { label: 'Inactivos', filterName: UserStatus.INACTIVE },
+    { label: 'Fuera del programa', filterName: UserStatus.OUT },
+    { label: 'No disponibles', filterName: UserStatus.NOT_AVAILABLE },
+  ];
+
   return (
     <>
       <CustomHead title="Mentores" />
@@ -56,7 +94,22 @@ const Mentors = () => {
           isDataEmpty={emptyMentors}
           noDataMessage="Aún no se han registrado mentores"
         >
-          {mentors.map(mentor => (
+          <div className="flex px-4 h-8 gap-2">
+            {filterButtons.map(button => (
+              <CustomButton
+                key={button.label}
+                bntLabel={button.label}
+                primary
+                clickAction={() =>
+                  onFilterChange(button.filterName as UserStatus | 'all')
+                }
+                isActive={getActiveButton(
+                  button.filterName as UserStatus | 'all',
+                )}
+              />
+            ))}
+          </div>
+          {filteredMentors.map(mentor => (
             <MentorCardSanity key={mentor._id} mentor={mentor} />
           ))}
         </GenericCard>
