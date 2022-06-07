8774110df2695ff39ea04305953e716254ea4cd9
Francisco Peralta <fran.mper@gmail.com>
2022-06-07 16:25:00 -0300

    delete changes on timeouts

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  timeout: 7000,
+  timeout: 5000,
 });
 
 export const axiosAWSInstance = axios.create({
   baseURL: process.env.AWS_BASE_URL,
-  timeout: 9000,
+  timeout: 7000,
   headers: {
     'x-api-key': process.env.AWS_API_KEY || '',
   },
